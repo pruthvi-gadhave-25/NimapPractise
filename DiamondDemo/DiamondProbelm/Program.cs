48b050d9991273a0fa9cb008f89f5286287b6dc7
Nimap <pruthvi.cdac@gmail.com>
2024-12-25 15:41:52 +0530

    list and diamond problem

---
+++ DiamondDemo/DiamondProbelm/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+
+namespace DiamondProblem
+{
+    //Base class A 
+    class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Print from A ");
+        }
+    }
+  
+    class B :A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Print from b ");
+        }
+    }
+    class C  : B
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Print from c ");
+        }
+    }
+
+    //so here we can inhertit B and C class from D 
+    //Compiler Error  
+    //  D cannot have multiple base classes 
+    //class D : B , C
+    //{
+
+    //}
+
+
+    public interface IAInterface
+    {
+        void Print();
+    }
+    public interface IBInterface
+    {
+        void Print();
+    }
+
+    public class D : IAInterface, IBInterface
+    {
+        public void Print()
+        {
+            Console.WriteLine("From D class");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            //using multiple inhritence 
+            //here if we create object from D  get conflict from which print mthod access
+
+
+            //using interface 
+            D d = new D();
+            d.Print();
+        }
+    }
+
+}
---
+++ List/ListDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListDemo 
+{
+   public class Progrm
+    {
+        public static void Main()
+        {
+            List<int> list = new List<int>();
+          
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(87);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Capacity);
+            Console.WriteLine(list[2]);
+            foreach (int i in list) Console.WriteLine(i);
+        }
+    }
+}
+
+//--Resized Dynamically but array can resize 
+//--it Allows Duplicate 
+//-- based index access element 
+
