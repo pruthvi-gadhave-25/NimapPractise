ba549df06851e24373f0dc18f67d7914bb4cd84d
Nimap <pruthvi.cdac@gmail.com>
2024-12-26 12:12:59 +0530

    var vs dynamic vs object

---
+++ VarVsDynamic/VarDynamicDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace VarDynamicDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            //var 
+            // -- Static  type ,check at compile time
+            // once assigned type cant be changed 
+
+            var num = 10;
+            num = 56; //allwed 
+            // var num = "jdbfh"; //this will give error 
+
+
+            //Dynamic 
+            // -- Types caheck at runtime time (i.e.Dynamic )
+            // -- type can be changed 
+
+            dynamic number = 100;
+            number = "Pruthvi"; //this is allowed in dynamic 
+
+
+            //Object  
+            // Base class of all class 
+            // -- Types caheck at runtime time 
+            // -- type can be changed but nedd explicit typecasting 
+
+            object first  = 10;
+            first  = "pruthvi"; //this is not chekd at compile time ,checked at runtime
+            
+            Console.WriteLine(((string)first).Length);
+            //Console.WriteLine(first.Length); //it will give compile time error Length not found method need explicit casting 
+
+
+
+
+
+        }
+    }
+}
