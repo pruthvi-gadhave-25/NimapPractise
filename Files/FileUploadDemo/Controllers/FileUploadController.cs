0cb9efb0f98e219c631fa5b4dabfd6c096ad6287
Nimap <pruthvi.cdac@gmail.com>
2025-01-27 19:32:04 +0530

    file upload

---
+++ Files/FileUploadDemo/Controllers/FileUploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace FileUploadDemo.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {   
+
+            return View();
+        }
+
+        public async Task<IActionResult> SingleFileUpload(IFormFile SingleFile)
+        {
+           if(ModelState.IsValid)
+            {
+
+                //using buffering 
+                if(SingleFile !=null || SingleFile.Length > 0)
+                {
+                    var filePath =  Path.Combine(Directory.GetCurrentDirectory(),"www/uploads" ,SingleFile.FileName);
+
+                    using(var stream = System.IO.File.Create(filePath))
+                    {
+                        await SingleFile.CopyToAsync(stream);
+                    }
+                    return View("UploadSuccess");
+                }
+            }
+            return View("Index");
+
+
+        }
+    }
+}
