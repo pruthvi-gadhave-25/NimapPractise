1ad5b5e7a2a86201f591e211b5bf9c218bc0c40e
pruthivirajcdac <pruthvi.cdac@gmail.com>
2025-07-09 00:16:49 +0530

    ef core problem

--- MvcEmpolyee/EmployeeCrud_Sat/Data/AppDbContext.cs
+++ MvcEmpolyee/EmployeeCrud_Sat/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeCrud_Sat.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeCrud_Sat.Data
@@ -9,9 +10,40 @@ namespace EmployeeCrud_Sat.Data
         {
         }
 
+        public DbSet<Country> Country { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<State> States { get; set; }
         public DbSet<Employee>Employees { get; set; }
-        public DbSet<Country>Country { get; set; }
-        public DbSet<City>Cities { get; set; }
-        public DbSet<State>States { get; set; }
+     
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Ignore<SelectListGroup>();
+            modelBuilder.Ignore <SelectListItem>();
+
+
+            //employee -  country
+            modelBuilder.Entity<Employee>() 
+                .HasOne(e => e.Country) //navigation prop 
+                .WithMany()
+                .HasForeignKey(e => e.CountryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Employee → State
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.State)
+                .WithMany()
+                .HasForeignKey(e => e.StateId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Employee → City
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.City)
+                .WithMany()
+                .HasForeignKey(e => e.CityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- MvcEmpolyee/EmployeeCrud_Sat/Models/City.cs
+++ MvcEmpolyee/EmployeeCrud_Sat/Models/City.cs
@@ -2,7 +2,7 @@
 {
     public class City
     {
-        public int Id { get; set; }
+        public int CityId { get; set; }
         public string Name { get; set; }
         public int StateId { get; set; }
         public State? State { get; set; }
